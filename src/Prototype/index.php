18165250b7d58504f8f5c1412bb595ceebcdadff
Oliver Amezquita Morales <varilla24@gmail.com>
2023-09-19 00:55:12 -0600

    Prototype and Singleton patterns were added

---
+++ src/Prototype/index.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace PerficientTest\Prototype;
+
+class Page
+{
+    private $title;
+
+    private $body;
+
+    /**
+     * @var Author
+     */
+    private $author;
+
+    private $comments = [];
+
+    /**
+     * @var \DateTime
+     */
+    private $date;
+
+    public function __construct(string $title, string $body, Author $author)
+    {
+        $this->title = $title;
+        $this->body = $body;
+        $this->author = $author;
+        $this->author->addToPage($this);
+        $this->date = new \DateTime();
+    }
+
+    public function addComment(string $comment): void
+    {
+        $this->comments[] = $comment;
+    }
+
+    public function __clone()
+    {
+        $this->title = "Copy of " . $this->title;
+        $this->author->addToPage($this);
+        $this->comments = [];
+        $this->date = new \DateTime();
+    }
+}
+
+class Author
+{
+    private $name;
+
+    /**
+     * @var Page[]
+     */
+    private $pages = [];
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function addToPage(Page $page): void
+    {
+        $this->pages[] = $page;
+    }
+}
+
+function clientCode()
+{
+    $author = new Author("John Smith");
+    $page = new Page("Tip of the day", "Keep calm and carry on.", $author);
+
+    $page->addComment("Nice tip, thanks!");
+
+    $draft = clone $page;
+    echo "Dump of the clone. Note that the author is now referencing two objects.\n\n";
+    print_r($draft);
+}
+
+clientCode();
